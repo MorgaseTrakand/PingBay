cbea5898643adcefacbb8c0486aa21fda801535e
Ethan Snyder <edsnyder1@geneva.edu>
2025-10-03 23:41:26 -0400

    Enhanced header componenet

--- apps/web/src/pages/Components/Header.tsx
+++ apps/web/src/pages/Components/Header.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useAuth from "@/hooks/useAuth";
 import { useUpdateUserState } from "@/lib/zustand";
+import { useLocation } from "react-router-dom";
 
 type Props = {};
 
 const Header: React.FC<Props> = () => {
   const isLoggedIn = useUpdateUserState((state) => state.isLoggedIn);
   
+  const location = useLocation();
+  const [loginPage, setLoginPage] = useState(false)
+  const [signupPage, setSignupPage] = useState(false)
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+      if (location) {
+          if (location.pathname == '/login') {
+            setLoginPage(true)
+          } else if (location.pathname == '/signup') {
+            setSignupPage(true)
+          }
+          setLoaded(true)
+      }
+  }, [location])
+
   return (
     <>
       <header className="w-[80%] absolute top-0 left-1/2 -translate-x-1/2 h-16 border border-border bg-background/80 backdrop-blur rounded-xl m-2">
@@ -21,19 +38,19 @@ const Header: React.FC<Props> = () => {
             </a>
 
           {/* Auth (right) */}
-          {!isLoggedIn && <nav className="flex items-center gap-3">
-            <a
+          {!isLoggedIn && loaded && <nav className="flex items-center gap-3">
+            {!loginPage && <a
               href="/login"
               className="rounded-md px-3 py-2 text-sm font-medium hover:bg-muted"
             >
               Log in
-            </a>
-            <a
+            </a>}
+            {!signupPage && <a
               href="/signup"
               className="rounded-md bg-primary px-3 py-2 text-sm font-semibold text-primary-foreground hover:opacity-90"
             >
               Sign up
-            </a>
+            </a>}
           </nav>}
           {isLoggedIn && <nav className="flex items-center gap-3">
             <a
